91e367db4d45fb31dfd6095987b936745ea5093c
kodinkat <karl.katewu@younty.com>
2024-01-31 20:38:23 +0000

    Added new ant sub-topics instead of single uber ant-pulse

--- src/modules/ant_pulse/ant_pulse.hpp
+++ src/modules/ant_pulse/ant_pulse.hpp
@@ -35,8 +35,79 @@
 
 #include <px4_platform_common/module.h>
 #include <px4_platform_common/module_params.h>
+#include <px4_platform_common/getopt.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/posix.h>
+
 #include <uORB/SubscriptionInterval.hpp>
+
+#include <uORB/topics/ant_actions.h>
+#include <uORB/topics/ant_actuators.h>
+#include <uORB/topics/ant_airspeeds.h>
+#include <uORB/topics/ant_arming.h>
+#include <uORB/topics/ant_battery.h>
+#include <uORB/topics/ant_camera.h>
+#include <uORB/topics/ant_cellular.h>
+#include <uORB/topics/ant_collisions.h>
+#include <uORB/topics/ant_cpu.h>
+#include <uORB/topics/ant_distances.h>
+#include <uORB/topics/ant_esc.h>
+#include <uORB/topics/ant_estimators.h>
+#include <uORB/topics/ant_failures.h>
+#include <uORB/topics/ant_health.h>
+#include <uORB/topics/ant_hovering.h>
+#include <uORB/topics/ant_landing.h>
+#include <uORB/topics/ant_lights.h>
+#include <uORB/topics/ant_logs.h>
+#include <uORB/topics/ant_power.h>
+#include <uORB/topics/ant_pulse.h>
+#include <uORB/topics/ant_radio.h>
+#include <uORB/topics/ant_sensors.h>
+
 #include <uORB/topics/parameter_update.h>
+#include <uORB/topics/action_request.h>
+#include <uORB/topics/actuator_armed.h>
+#include <uORB/topics/actuator_motors.h>
+#include <uORB/topics/airspeed.h>
+#include <uORB/topics/airspeed_validated.h>
+#include <uORB/topics/airspeed_wind.h>
+#include <uORB/topics/arming_check_reply.h>
+#include <uORB/topics/battery_status.h>
+#include <uORB/topics/camera_status.h>
+#include <uORB/topics/cellular_status.h>
+#include <uORB/topics/collision_report.h>
+#include <uORB/topics/cpuload.h>
+#include <uORB/topics/distance_sensor.h>
+#include <uORB/topics/esc_report.h>
+#include <uORB/topics/esc_status.h>
+#include <uORB/topics/estimator_innovations.h>
+#include <uORB/topics/estimator_status_flags.h>
+#include <uORB/topics/failsafe_flags.h>
+#include <uORB/topics/failure_detector_status.h>
+#include <uORB/topics/health_report.h>
+#include <uORB/topics/hover_thrust_estimate.h>
+#include <uORB/topics/landing_gear.h>
+#include <uORB/topics/led_control.h>
+#include <uORB/topics/log_message.h>
+#include <uORB/topics/mavlink_log.h>
+#include <uORB/topics/obstacle_distance.h>
+#include <uORB/topics/onboard_computer_status.h>
+#include <uORB/topics/power_button_state.h>
+#include <uORB/topics/power_monitor.h>
+#include <uORB/topics/radio_status.h>
+#include <uORB/topics/sensor_accel.h>
+#include <uORB/topics/sensor_baro.h>
+#include <uORB/topics/sensor_gyro.h>
+#include <uORB/topics/sensor_hygrometer.h>
+#include <uORB/topics/sensor_mag.h>
+#include <uORB/topics/sensor_optical_flow.h>
+#include <uORB/topics/system_power.h>
+#include <uORB/topics/takeoff_status.h>
+#include <uORB/topics/vehicle_land_detected.h>
+#include <uORB/topics/wind.h>
+
+#include <algorithm>
+#include <iterator>
 
 using namespace time_literals;
 
